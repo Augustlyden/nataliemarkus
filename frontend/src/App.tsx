ea5299afca497f8b6a58c366738a04e313b47659
Augustlyden <august.lyden@outlook.com>
2026-09-19 20:51:10 +0200

    feat: implement SaveTheDatePage routing and update font styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import SaveTheDatePage from './pages/SaveTheDatePage'
 
 function App() {
-  
-
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SaveTheDatePage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
